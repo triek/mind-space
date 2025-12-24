2ce8ecca7a114f3212b37ee3a4d6e133bf3a38bf
Tristan Khuong <87681993+triek@users.noreply.github.com>
2025-12-24 12:21:02 +0000

    setup vue dev tools

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default {
   plugins: [
     vue(),
+    vueDevTools(),
     tailwindcss(),
   ],
 }
