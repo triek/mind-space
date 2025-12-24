870859c7f02f0be5e82190256f6bba84d686854e
Tristan Khuong <87681993+triek@users.noreply.github.com>
2025-12-24 12:26:01 +0000

    add gh-page deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default {
+  base: '/mind-space',
   plugins: [
     vue(),
     vueDevTools(),
