37e39820971678bc13ff66f8b0e79285fdd88ecf
Tristan Khuong <87681993+triek@users.noreply.github.com>
2025-12-24 12:19:53 +0000

    initialize tailwind

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,18 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
+    
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
+
   <HelloWorld msg="Vite + Vue" />
+
+  <div class="bg-black text-white p-6">
+    Tailwind is working
+  </div>
+
 </template>
 
 <style scoped>
