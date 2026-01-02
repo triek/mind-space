749b753f29404026b1f298df7931af9c5e2b1dda
Tristan Khuong <87681993+triek@users.noreply.github.com>
2026-01-02 02:52:18 +0000

    added quiz page rule

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 <script setup>
 import { computed, ref } from 'vue'
 import HomePage from './components/HomePage.vue'
+import CheckInQuizPage from './components/CheckInQuizPage.vue'
 import NewConsultationPage from './components/NewConsultationPage.vue'
 import ThreadDetailPage from './components/ThreadDetailPage.vue'
 
@@ -14,6 +15,11 @@ const activeComponent = computed(() => {
   if (activePage.value === 'new-thread') {
     return NewConsultationPage
   }
+
+  if (activePage.value === 'check-in-quiz') {
+    return CheckInQuizPage
+  }
+
   if (activePage.value === 'thread-detail') {
     return ThreadDetailPage
   }
